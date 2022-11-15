2fe8c3da9ff854897b447acf9f0f9c38a5d564fa
zHazyl <tnh210302@gmail.com>
2022-11-15 16:56:15 +0700

    done docker

--- auth-service/target/generated-sources/annotations/com/tnh/authservice/mapper/UserMapperImpl.java
+++ auth-service/target/generated-sources/annotations/com/tnh/authservice/mapper/UserMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-11-09T15:31:47+0700",
-    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 11.0.16 (Debian)"
+    date = "2022-11-15T16:25:42+0700",
+    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 17.0.4 (Debian)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
