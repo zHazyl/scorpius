897dccdb8370cf4855ed81dc0ec43036b69db3c6
zHazyl <tnh210302@gmail.com>
2022-12-15 15:07:32 +0700

    update redis info

--- chat-service/src/main/java/com/tnh/chatservice/web/rest/FriendChatController.java
+++ chat-service/src/main/java/com/tnh/chatservice/web/rest/FriendChatController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import com.tnh.chatservice.dto.FriendChatDTO;
 import com.tnh.chatservice.mapper.FriendChatMapper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -43,15 +44,29 @@ public class FriendChatController {
             friendChatRedisList = friendChatService.getAllFriendChatsRedisBySender(SecurityUtils.getCurrentUser());
             if (!friendChatRedisList.isEmpty()) {
                 logger.debug("Redis");
-                return ResponseEntity.ok((List<FriendChatDTO>) friendChatRedisList.stream().map(friendChatRedis -> {
+
+                List<FriendChatDTO> friendChatDTOS = new ArrayList<>();
+                friendChatRedisList.forEach(friendChatRedis -> {
                     FriendChatDTO friendChatDTO = new FriendChatDTO();
                     friendChatDTO.setId(friendChatRedis.getId());
                     friendChatDTO.setChatWith(friendChatRedis.getChatWith());
                     ChatProfileDTO chatProfileDTO = new ChatProfileDTO();
                     chatProfileDTO.setUserId(friendChatRedis.getRecipient());
                     friendChatDTO.setRecipient(chatProfileDTO);
-                    return friendChatDTO;
-                }));
+                    friendChatDTOS.add(friendChatDTO);
+                });
+
+                return ResponseEntity.ok(friendChatDTOS);
+
+//                return ResponseEntity.ok((List<FriendChatDTO>) friendChatRedisList.stream().map(friendChatRedis -> {
+//                    FriendChatDTO friendChatDTO = new FriendChatDTO();
+//                    friendChatDTO.setId(friendChatRedis.getId());
+//                    friendChatDTO.setChatWith(friendChatRedis.getChatWith());
+//                    ChatProfileDTO chatProfileDTO = new ChatProfileDTO();
+//                    chatProfileDTO.setUserId(friendChatRedis.getRecipient());
+//                    friendChatDTO.setRecipient(chatProfileDTO);
+//                    return friendChatDTO;
+//                }));
             }
         } catch (Exception e) {
 
